65ff7a1de9cb94649507c6278d5a1ff103b5619b
Jesse <jesseprescott@yahoo.com>
2021-03-05 19:09:11 -0500

    Add project files.

---
+++ BlazorDevExpressGridDemo/MyDbContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorDevExpressGridDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorDevExpressGridDemo
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+            if (!Departments.Any())
+            {
+                Departments.AddRange(new Department { Id = 1, Name = "Department1" }, new Department { Id = 2, Name = "Department2" });
+            }
+
+            if (!Roles.Any())
+            {
+                Roles.AddRange(new Role { Id = 1, Name = "Role1" }, new Role { Id = 2, Name = "Role2" });
+            }
+
+            SaveChanges();
+        }
+        
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        
+    }
+
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Department>().HasData(
+                new Department { Id = 1, Name = "Dep1" },
+                new Department { Id = 2, Name = "Dep2" }
+            );
+
+            modelBuilder.Entity<Role>().HasData(
+                new Role { Id = 1, Name = "Role1" },
+                new Role { Id = 2, Name = "Role2" }
+            );
+        }
+    }
+}
